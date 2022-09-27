20438b1ca0f413802fd63c482b9a20431e77f87b
Rickey Morris <rmorris.rickey@gmail.com>
2022-09-27 17:08:07 -0400

    Now validates user input to check for a positive integer between 1 and 100. Resolved issue where final row of squares in the sketchpad was not getting event listeners of mouseover and click by moving the block of code begining with 'const squares = document.querySelectAll(.sqare)' outside of the nested for loop in the 'createGrid()' function.

--- myScript.js
+++ myScript.js
@@ -1,3 +1,7 @@
+/*
+TODO
+Need to sanitize user input so that only numbers 1 through 100 are accepted.
+*/
 let container = document.querySelector("div.container");
 let button = document.querySelector("button");
 let size;
@@ -11,6 +15,15 @@ function getSize(){
         }
     }
     size = prompt("Size your grid");
+    //'size' currenlty stores a string type and needs to be converted to a Number type
+    //for the boolean evaluation. 'Number(size)' will return NaN to 'Number.isInteger()'
+    //if the value stored in 'size' cannot be converted to a number.
+    while(!Number.isInteger(Number(size)) || (size > 100 || size <= 0)){
+        size = prompt("Please enter a whole number between 1 and 100.");
+    }
+    // if(size < 0){
+    //     size = prompt("Value must be greater than 0 and no more than 100.");
+    // }
     createGrid(size);
 }
 
@@ -19,13 +32,17 @@ for (let rowNum = 1; rowNum <= size; rowNum++){
     //I can create nested for loop here to populate each containter with the number of items specified.
     //Each row can be seen as a flex container, containing sqaures which are its flex items.
     let row = document.createElement("div");
-    for (let squareNum = 1; squareNum <=size; squareNum++){
+    for (let squareNum = 1; squareNum <= size; squareNum++){
         let square = document.createElement("div");
         square.classList.add("square");
         row.appendChild(square);
     }
     row.classList.add("row");
-    const squares = document.querySelectorAll(".square");
+    container.appendChild(row);
+}
+
+const squares = document.querySelectorAll(".square");
+    console.log(squares)
     squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "black";
@@ -34,6 +51,12 @@ for (let rowNum = 1; rowNum <= size; rowNum++){
             square.style.backgroundColor = "white";
         })
     })
-    container.appendChild(row);
 }
-}
+
+/*
+Ideas for future iterations
+-Add the ability to undo colored squares. This can be accomplished by a mouse event (click and hold) or by
+creating a button that serves as a toggle between fill and erase.
+-Add a reset button that clears the entire grid while keeping its size.
+-Let the user swap between colors.
+*/
