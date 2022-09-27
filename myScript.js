4ca3871fc11927060ef454f1e2a6b111eca2f71c
Rickey Morris <rmorris.rickey@gmail.com>
2022-09-27 13:40:24 -0400

    Added functions 'getSize' and 'createGrid' which prompts for user input and creates the resultinggrid respectively. 'getSize' removes any previous grids by checking to the container element for any children (the created div tags for the squares).

--- myScript.js
+++ myScript.js
@@ -1,15 +1,39 @@
 let container = document.querySelector("div.container");
+let button = document.querySelector("button");
+let size;
 
-for (let rowNum = 1; rowNum <= 16; rowNum++){
+button.addEventListener("click", getSize)
+
+function getSize(){
+    if(container.hasChildNodes()){
+        while(container.hasChildNodes()){
+            container.removeChild(container.firstChild);
+        }
+    }
+    size = prompt("Size your grid");
+    createGrid(size);
+}
+
+function createGrid(size){
+for (let rowNum = 1; rowNum <= size; rowNum++){
     //I can create nested for loop here to populate each containter with the number of items specified.
+    //Each row can be seen as a flex container, containing sqaures which are its flex items.
     let row = document.createElement("div");
-    for (let squareNum = 1; squareNum <=16; squareNum++){
+    for (let squareNum = 1; squareNum <=size; squareNum++){
         let square = document.createElement("div");
-        // square.textContent = squareNum;
         square.classList.add("square");
         row.appendChild(square);
     }
     row.classList.add("row");
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        })
+        square.addEventListener("click", () => {
+            square.style.backgroundColor = "white";
+        })
+    })
     container.appendChild(row);
-    console.log(`Row: ${rowNum}`);
+}
 }
